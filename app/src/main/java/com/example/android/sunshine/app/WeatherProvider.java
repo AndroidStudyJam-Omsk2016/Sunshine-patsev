1e0ca98af6f1b267542c4e945a0bdd5eea5b7ab4
Anton Patsev <patsev.anton@gmail.com>
2016-05-07 13:12:15 +0600

    Insert : coding the ContentProvider

--- app/src/main/java/com/example/android/sunshine/app/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/WeatherProvider.java
@@ -240,6 +240,14 @@ public class WeatherProvider extends ContentProvider {
                     throw new android.database.SQLException("Failed to insert row into " + uri);
                 break;
             }
+            case LOCATION: {
+                long _id = db.insert(com.example.android.sunshine.app.data.WeatherContract.LocationEntry.TABLE_NAME, null, values);
+                if ( _id > 0 )
+                    returnUri = com.example.android.sunshine.app.data.WeatherContract.LocationEntry.buildLocationUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            }
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
